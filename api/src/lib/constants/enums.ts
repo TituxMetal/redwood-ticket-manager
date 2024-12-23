11774d90486bdea3bba0f84433c2b032ec2f2118
Titux Metal (DEV) <github@lgdweb.fr>
2024-12-23 03:46:09 +0100

    feat(types): add ticket status enum and type

--- api/src/lib/constants/enums.ts
+++ api/src/lib/constants/enums.ts
@@ -5,3 +5,11 @@ export const TicketPriority = {
 } as const
 
 export type TicketPriorityType = keyof typeof TicketPriority
+
+export const TicketStatus = {
+  OPEN: 'OPEN',
+  IN_PROGRESS: 'IN_PROGRESS',
+  CLOSED: 'CLOSED'
+} as const
+
+export type TicketStatusType = keyof typeof TicketStatus
